12b219da272f27b2c83af3fa10d179183eaba9c9
hengzhang <997635172@qq.com>
2017-06-28 23:30:03 +0800

    C 与指针第一章示例代码
    2017年6月28日23:29:24
    复习C语言

---
+++ Chapter01/1.1.cpp
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX_COLS 20
+#define MAX_INPUT 1000
+
+int read_column_numbers(int columns[], int max);
+void rerange(char *output,char const *input,int n_column,int const columns[]);
+
+int main()
+{
+	int n_columns;
+	int columns[MAX_COLS];
+	char input[MAX_INPUT];
+	char output[MAX_INPUT];
+
+	n_columns = read_column_numbers(columns,MAX_COLS);
+
+	while (fgets(input,MAX_INPUT,stdin) != NULL)
+	{
+		printf("original input:%s", input);
+		rerange(output,input,n_columns,columns);
+		printf("reranged output:%s", output);
+	}
+
+
+	return 0;
+}
+
+/*
+* 读取列标号，若超出范围则不理会
+*/
+int read_column_numbers(int columns[],int max)
+{
+	int num = 0;
+	int ch;
+
+	while (num < max && scanf("%d", &columns[num]) == 1 && columns[num] >= 0)
+		num += 1;
+
+	if (num % 2 != 0)
+	{
+		puts("Last column number is not paried");
+		exit(EXIT_FAILURE);
+	}
+
+	while ((ch = getchar()) != EOF && ch != '\n')
+		;
+	return num;
+}
+
+
+void rerange(char *output,char const *input,int n_columns,int const columns[])
+{
+	int col;
+	int output_col;
+	int len;
+
+	len = strlen(input);
+	output_col = 0;
+
+	for (col = 0; col < n_columns; col += 2)
+	{
+		int nchars = columns[col + 1] - columns[col] + 1;
+		if (columns[col] >= len || output_col == MAX_INPUT - 1)
+			break;
+		if (output_col + nchars > MAX_INPUT - 1)
+			nchars = MAX_INPUT - output_col - 1;
+
+		strncpy(output+output_col,input+columns[col],nchars);
+		output_col += nchars;
+	}
+
+	output[output_col] = '\0';
+}
